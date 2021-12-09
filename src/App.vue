f83c25d3d01c93c96e82eb53ae9df5fbbe42c933
mrLittleBoner <fruit142@gmail.com>
2021-12-09 19:41:18 +0300

    add AList component and structure for ListView in App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,96 @@
 <template>
-  Ghbdtn
+  <div class="lists">
+    <AList
+      v-for="(list,index) in lists" :key="index"
+      :listNumber="list.id" :items="list.items"
+      @changeColor="changeColor"
+    />
+  </div>
+  <div class="lists-view">
+    <ul class="lists-view__list" v-for="list in lists" :key="list.id">
+      <li class="list-view__title">List {{ list.id }}</li>
+      <li class="lists-view__item" v-for="(item,index) in list.items" :key="index">
+        <span class="list-view__square" :style="{'background-color': item.color}" v-for="(n) in item.count" :key="n"></span>
+      </li>
+    </ul>
+  </div>  
 </template>
 
 <script>
-
+import AList from './components/AList.vue'
+import { ref } from 'vue'
 export default {
-  name: 'App',
+  components: { AList },
+  setup() {
+    const lists = ref([
+      {id:1, 
+        items:[
+          {count:4, color:'#ffffff'},
+          {count:7, color:'#000000'},
+          {count:12, color:'#ff00e1'},
+          {count:5, color:'#00c9b2'},
+          {count:2, color:'#333333'}
+        ]
+      }
+    ])
+
+    function changeColor(listId,itemInedx,newColor) {
+      lists.value.map(list => {
+        if(list.id === listId) {
+          list.items[itemInedx].color = newColor
+          console.log(listId,itemInedx,newColor)
+        }
+      })
+    }
+
+    return {
+      lists,changeColor
+    }
+  },
 }
 </script>
 
 <style>
+*,*::after,*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+ul {
+  list-style-type: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  padding: 20px;
+
+  display: flex;
+  justify-content: space-between;
+}
+.lists,
+.lists-view {
+  border: 1px solid tomato;
+  width: 40%;
+  padding: 20px;
+}
+
+.lists-view__list {
+  border: 1px solid tomato;
+  padding: 10px;
+}
+
+.lists-view__item {
+  background-color: #eee;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
+  padding: 5px;
+}
+.list-view__square {
+  width: 12px;
+  height: 12px;
+  display: inline-block;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/AList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="list">
+    <div class="list__title">
+      <input type="checkbox">
+      <h3>List {{ listNumber }}</h3>
+    </div>
+
+    <ul class="list__items">
+      <li class="list__item" v-for="(item,index) in items" :key="index">
+        <label>
+          <input type="checkbox">
+          <span>Item {{ index+1 }}</span>
+        </label>
+        <div class="list__item-controls">
+          <span class="list__item-count">{{ item.count }}</span>
+          <input
+            class="list__item-color"
+            type="color"
+            v-model="item.color"
+            @change="(e) => $emit('changeColor', listNumber,index, e.target.value)"
+          >
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  emits:['changeColor'],
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    listNumber: {
+      type: Number,
+      required: true
+    }
+  },
+  setup(){
+    function showmeevent(e){
+      console.log(e)
+    }
+    return { showmeevent }
+  }
+}
+</script>
+
+<style scoped>
+
+.list__title {
+  display: flex;
+  gap: 25px;
+  align-items: center;
+  margin-bottom: 15px;
+}
+.list__items {
+  list-style-type: none;
+  margin-left: 35px;
+}
+
+.list__item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.list__item-controls,
+.list__item label {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+</style>
